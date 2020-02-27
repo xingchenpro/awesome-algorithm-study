468f337648deb4695bf35c033be856af9bc4df3f
黄良运 <1136513099@qq.com>
2020-02-27 21:27:43 +0800

    剑指offer 3,4,5

---
+++ src/com/hly/leetCode/offer/ReversePrint_06.java
@@ -0,0 +1,42 @@
+package com.hly.leetCode.offer;
+
+/**
+ * @author :hly
+ * @github :https://github.com/huangliangyun
+ * @blog :http://www.javahly.com/
+ * @CSDN :blog.csdn.net/Sirius_hly
+ * @date :2020/2/27
+ * @QQ :1136513099
+ * @desc : 面试题06. 从尾到头打印链表
+ * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ * 输入：head = [1,3,2]
+ * 输出：[2,3,1]
+ * 要点：
+ */
+public class ReversePrint_06 {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+    public int[] reversePrint(ListNode head) {
+
+return null;
+
+
+    }
+
+
+
+    public static void main(String[] args){
+        System.out.println("");
+    }
+
+
+
+
+}
+
