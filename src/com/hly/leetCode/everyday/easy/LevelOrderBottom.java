3d83f8b25a36d70d6718a7e9713e55b2a591aa99
黄良运 <1136513099@qq.com>
2020-06-25 23:55:16 +0800

    6.22 3道

--- src/com/hly/leetCode/everyday/easy/LevelOrderBottom.java
+++ src/com/hly/leetCode/everyday/easy/LevelOrderBottom.java
@@ -1,6 +1,9 @@
 package com.hly.leetCode.everyday.easy;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * @author :hly
@@ -8,8 +11,8 @@ import java.util.List;
  * @CSDN :blog.csdn.net/Sirius_hly
  * @WeChat :liangyun2021
  * @date :2020/6/25
- * @desc : 107. 二叉树的层次遍历 II
- *  https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii/
+ * @desc : 107. 二叉树的层次遍历 II/
+ * https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii/
  */
 public class LevelOrderBottom {
 
@@ -22,8 +25,32 @@ public class LevelOrderBottom {
             this.val = val;
         }
     }
-    public List<List<Integer>> levelOrderBottom(TreeNode root) {
 
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        LinkedList<List<Integer>> result = new LinkedList<>();
+        //判断是否为空，返回结果
+        if (root == null)
+            return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                //添加每一层的元素
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            //每次往队列头部插入
+            result.addFirst(list);
+        }
+        return result;
     }
 
 
