3a8adcfa79c5818209fb4b7cac8aca4ecdd5737d
黄良运 <1136513099@qq.com>
2020-07-30 22:08:13 +0800

    7.29 3道

--- src/com/hly/leetCode/everyday/easy/WordPattern.java
+++ src/com/hly/leetCode/everyday/easy/WordPattern.java
@@ -6,7 +6,15 @@ package com.hly.leetCode.everyday.easy;
  * @CSDN :blog.csdn.net/Sirius_hly
  * @WeChat :liangyun2021
  * @date :2020/7/29
- * @desc :
+ * @desc : 290. 单词规律
+ * https://leetcode-cn.com/problems/word-pattern/
  */
 public class WordPattern {
+
+    public boolean wordPattern(String pattern, String str) {
+
+    }
+    public static void main(String[] args){
+
+    }
 }
