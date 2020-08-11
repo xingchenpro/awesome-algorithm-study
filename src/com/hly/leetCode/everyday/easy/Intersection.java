011b037e8eaa1f696c40d9b2610409ca341a5eb5
黄良运 <1136513099@qq.com>
2020-08-11 22:37:27 +0800

    8.11 3道

--- src/com/hly/leetCode/everyday/easy/Intersection.java
+++ src/com/hly/leetCode/everyday/easy/Intersection.java
@@ -1,5 +1,9 @@
 package com.hly.leetCode.everyday.easy;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * @author :hly
  * @github :https://github.com/huangliangyun
@@ -11,7 +15,31 @@ package com.hly.leetCode.everyday.easy;
  */
 public class Intersection {
     public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> setRes = new HashSet<>();
+
+        for (int i : nums1) {
+            set.add(i);
+        }
+        for (int i : nums2) {
+            if (set.contains(i)) {
+                setRes.add(i);
+            }
+        }
+        int[] res = new int[setRes.size()];
+        Iterator<Integer> iterator = setRes.iterator();
+        int i = 0;
+        while (iterator.hasNext()) {
+            res[i++] = iterator.next();
+        }
+        return res;
     }
+
     public static void main(String[] args) {
+
+        int[] res = new Intersection().intersection(new int[]{1, 2, 2, 1}, new int[]{2, 2});
+        for (int re : res) {
+            System.out.print(re + " ");
+        }
     }
 }
---
+++ src/com/hly/leetCode/everyday/easy/IsPerfectSquare.java
@@ -0,0 +1,43 @@
+package com.hly.leetCode.everyday.easy;
+
+/**
+ * @author :hly
+ * @github :https://github.com/huangliangyun
+ * @CSDN :blog.csdn.net/Sirius_hly
+ * @WeChat :liangyun2021
+ * @date :2020/8/11
+ * @desc : 367. 有效的完全平方数
+ * https://leetcode-cn.com/problems/valid-perfect-square/
+ */
+public class IsPerfectSquare {
+
+
+    public boolean isPerfectSquare(int num) {
+        if (num < 2) {
+            return true;
+        }
+        //long
+        long left = 2, right = num / 2, x, guess;
+        while (left <= right) {
+            x = left + (right - left) / 2;
+            guess = x * x;
+            if (guess == num) {
+                return true;
+            } else if (guess > num) {
+                right = x - 1;
+            } else {
+                left = x + 1;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new IsPerfectSquare().isPerfectSquare(16));//true
+        System.out.println(new IsPerfectSquare().isPerfectSquare(14));//false
+        System.out.println(new IsPerfectSquare().isPerfectSquare(1));//true
+        System.out.println(new IsPerfectSquare().isPerfectSquare(100));//true
+    }
+
+
+}
