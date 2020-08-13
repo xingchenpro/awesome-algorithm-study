30b81051ec980dc30c9c451898fe041ff2aaea7f
黄良运 <1136513099@qq.com>
2020-08-13 23:25:33 +0800

    8.12 3道

---
+++ src/com/hly/leetCode/everyday/easy/CanConstruct.java
@@ -0,0 +1,22 @@
+package com.hly.leetCode.everyday.easy;
+
+/**
+ * @author :hly
+ * @github :https://github.com/huangliangyun
+ * @CSDN :blog.csdn.net/Sirius_hly
+ * @WeChat :liangyun2021
+ * @date :2020/8/13
+ * @desc : 383. 赎金信
+ * https://leetcode-cn.com/problems/ransom-note/
+ */
+public class CanConstruct {
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
