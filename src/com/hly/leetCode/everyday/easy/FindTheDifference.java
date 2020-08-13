5e58c14688b5c4ab8749c84d1b00b0cb21295326
黄良运 <1136513099@qq.com>
2020-08-14 00:28:13 +0800

    8.12 3道

--- src/com/hly/leetCode/everyday/easy/FindTheDifference.java
+++ src/com/hly/leetCode/everyday/easy/FindTheDifference.java
@@ -1,5 +1,8 @@
 package com.hly.leetCode.everyday.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author :hly
  * @github :https://github.com/huangliangyun
@@ -12,11 +15,23 @@ package com.hly.leetCode.everyday.easy;
 public class FindTheDifference {
 
     public char findTheDifference(String s, String t) {
-
+        int[] h = new int[26];
+        for (int i = 0; i < t.length(); i++) {
+            h[t.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            h[s.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            if (h[t.charAt(i) - 'a'] > 0) {
+                return t.charAt(i);
+            }
+        }
+        return ' ';
     }
 
     public static void main(String[] args) {
-
+        System.out.println(new FindTheDifference().findTheDifference("abcd", "abcde"));
 
     }
 }
