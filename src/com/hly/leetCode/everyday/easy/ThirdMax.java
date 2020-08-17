7e7c16358c8f82858180dfe7b02bca9ca3392461
黄良运 <1136513099@qq.com>
2020-08-17 21:29:54 +0800

    8.15 3道

---
+++ src/com/hly/leetCode/everyday/easy/ThirdMax.java
@@ -0,0 +1,31 @@
+package com.hly.leetCode.everyday.easy;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * @author :hly
+ * @github :https://github.com/huangliangyun
+ * @CSDN :blog.csdn.net/Sirius_hly
+ * @WeChat :liangyun2021
+ * @date :2020/8/17
+ * @desc : 414. 第三大的数
+ * https://leetcode-cn.com/problems/third-maximum-number/
+ */
+public class ThirdMax {
+
+    public int thirdMax(int[] nums) {
+        int a,b,c;
+        for (int i=0;i<nums.length;i++){
+
+        }
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
