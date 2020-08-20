4f53830f9059299bf9a541f7719fc0b3e15ea8ff
黄良运 <1136513099@qq.com>
2020-08-20 22:59:16 +0800

    8.17 3道

---
+++ src/com/hly/leetCode/everyday/easy/MinMoves.java
@@ -0,0 +1,22 @@
+package com.hly.leetCode.everyday.easy;
+
+/**
+ * @author :hly
+ * @github :https://github.com/huangliangyun
+ * @CSDN :blog.csdn.net/Sirius_hly
+ * @WeChat :liangyun2021
+ * @date :2020/8/19
+ * @desc : 453. 最小移动次数使数组元素相等
+ * https://leetcode-cn.com/problems/minimum-moves-to-equal-array-elements/
+ */
+public class MinMoves {
+
+    public int minMoves(int[] nums) {
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
