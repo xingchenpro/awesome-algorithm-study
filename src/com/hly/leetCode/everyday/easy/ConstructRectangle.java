baf4f4a55efbc274601b7b00a5bfca24cec5ab4f
黄良运 <1136513099@qq.com>
2020-08-28 21:51:00 +0800

    8.23 3道

--- src/com/hly/leetCode/everyday/easy/ConstructRectangle.java
+++ src/com/hly/leetCode/everyday/easy/ConstructRectangle.java
@@ -9,6 +9,7 @@ package com.hly.leetCode.everyday.easy;
  * @desc : 492. 构造矩形
  * https://leetcode-cn.com/problems/construct-the-rectangle/
  */
+
 public class ConstructRectangle {
 
     public int[] constructRectangle(int area) {
@@ -22,7 +23,8 @@ public class ConstructRectangle {
     public static void main(String[] args) {
         int[] res = new ConstructRectangle().constructRectangle(4);//2,2
         int[] res1 = new ConstructRectangle().constructRectangle(1);//1,1
-        for (int re : res1) {
+        int[] res3 = new ConstructRectangle().constructRectangle(6);//1,1
+        for (int re : res3) {
             System.out.print(re + " ");
         }
 
