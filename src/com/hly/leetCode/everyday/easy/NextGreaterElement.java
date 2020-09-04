35aaf7026eee5f8bf32b1043f7c50d3fda3e0946
黄良运 <1136513099@qq.com>
2020-09-04 20:32:47 +0800

    8.23 3道

--- src/com/hly/leetCode/everyday/easy/NextGreaterElement.java
+++ src/com/hly/leetCode/everyday/easy/NextGreaterElement.java
@@ -10,12 +10,13 @@ import java.util.Stack;
  * @CSDN :blog.csdn.net/Sirius_hly
  * @WeChat :liangyun2021
  * @date :2020/9/2
- * @desc : 496. 下一个更大元素 I
+ * @desc : TODO 496. 下一个更大元素 I
  * https://leetcode-cn.com/problems/next-greater-element-i/
  */
 public class NextGreaterElement {
 
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        //栈底元素最大
         Stack<Integer> stack = new Stack<>();
         Map<Integer,Integer> map = new HashMap<>();
         int[] res = new int[nums1.length];
@@ -33,7 +34,7 @@ public class NextGreaterElement {
     }
 
     public static void main(String[] args) {
-
-
+        //[4,2,1],[1,3,4,2]？【1,->3】 kkkk数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素
+        //[4,1,2],[1,3,4,2]
     }
 }
