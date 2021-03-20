a21a98b7dd39f0c6172aac1b36d25e11312a44e7
huangliangyun <1136513099@qq.com>
2021-03-20 18:59:49 +0800

    10.17 1道

--- src/com/hly/leetCode/everyday/easy/MaxCount.java
+++ src/com/hly/leetCode/everyday/easy/MaxCount.java
@@ -4,7 +4,6 @@ package com.hly.leetCode.everyday.easy;
  * @author :hly
  * @github :https://github.com/huangliangyun
  * @CSDN :blog.csdn.net/Sirius_hly
- * @WeChat :liangyun2021
  * @date :2020/10/19
  * @desc : 598. 范围求和 II
  *  https://leetcode-cn.com/problems/range-addition-ii/
