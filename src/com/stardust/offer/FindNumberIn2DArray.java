9b7d3d1dbf9f4a309a63bd58a3d513777b5b56db
huangliangyun <1136513099@qq.com>
2021-04-15 00:41:34 +0800

    1

---
+++ src/com/stardust/offer/FindNumberIn2DArray.java
@@ -0,0 +1,52 @@
+package com.stardust.offer;
+
+/**
+ * @author :hly
+ * @github :https://github.com/huangliangyun
+ * @CSDN :blog.csdn.net/Sirius_hly
+ * @WeChat :xcvip2021
+ * @date :2021/4/14
+ * @desc : 04. 二维数组中的查找
+ * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ * 请完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ */
+public class FindNumberIn2DArray {
+
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        //判断
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int row = 0, col = matrix[0].length - 1;
+        //临界条件 >=0
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                row++;
+            } else if (matrix[row][col] > target) {
+                col--;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int matrix[][] = {
+                {1, 4, 7, 11, 15},
+                {2, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+        };
+
+        int matrix2[][] = {
+                {-5},
+        };
+
+        System.out.println(new FindNumberIn2DArray().findNumberIn2DArray(matrix, 5));
+        System.out.println(new FindNumberIn2DArray().findNumberIn2DArray(matrix, 11));
+        System.out.println(new FindNumberIn2DArray().findNumberIn2DArray(matrix2, -5));//true
+    }
+}
--- src/com/stardust/offer/FindRepeatNumber.java
+++ src/com/stardust/offer/FindRepeatNumber.java
@@ -1,5 +1,8 @@
 package com.stardust.offer;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author :hly
  * @github :https://github.com/huangliangyun
@@ -16,15 +19,27 @@ package com.stardust.offer;
  * 输出：2 或 3
  */
 public class FindRepeatNumber {
+
     public int findRepeatNumber(int[] nums) {
 
+        Set<Integer> set = new HashSet<>();
+        int repeat = -1;
+        for (int i = 0; i < nums.length; i++) {
+            // add 底层为 HashMap 的 put 方法
+            //如果已经存在一个相同的 key， 则返回的是前一个key对应的value，同时该key的新value覆盖旧value；
+            //如果是新的一个key，则返回的是null
+            //return map.put(e, PRESENT)==null;
+            //不是 null ,表示 为 false ,即存在元素
+           if(!set.add(nums[i])){
+               repeat = nums[i];
+           }
+        }
+        return  repeat;
+
     }
 
-    public static void main(String[] args){
-
-
+    public static void main(String[] args) {
+        System.out.println(new FindRepeatNumber().findRepeatNumber(new  int[]{2, 3, 1, 0, 2, 5, 3}));
     }
-
-
 
 }
