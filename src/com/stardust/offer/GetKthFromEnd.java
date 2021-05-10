ae37f81fd436ec6079d728fafabfee5461401cae
huangliangyun <1136513099@qq.com>
2021-05-10 22:00:00 +0800

    5.10

---
+++ src/com/stardust/offer/GetKthFromEnd.java
@@ -0,0 +1,51 @@
+package com.stardust.offer;
+
+/**
+ * @author :hly
+ * @github :https://github.com/huangliangyun
+ * @CSDN :blog.csdn.net/Sirius_hly
+ * @WeChat :xcvip2021
+ * @date :2021/5/10
+ * @desc : 22. 链表中倒数第k个节点
+ * 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+ * 例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+ */
+public class GetKthFromEnd {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        int size = 0;
+        ListNode cur = head;
+        while (cur != null) {
+            cur = cur.next;
+            size++;
+        }
+        int index = 0;
+        int n = size - k;
+        while (head != null && index < n) {
+            head = head.next;
+            index++;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        System.out.println(new GetKthFromEnd().getKthFromEnd(head, 3).val);
+
+    }
+
+}
