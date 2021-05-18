e5f873d66ebf0c7fa03b4e0a015e3f7764343ccb
huangliangyun <1136513099@qq.com>
2021-05-18 22:56:22 +0800

    5.18

---
+++ src/com/stardust/offer/LevelOrder.java
@@ -0,0 +1,59 @@
+package com.stardust.offer;
+
+import sun.reflect.generics.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author :hly
+ * @github :https://github.com/huangliangyun
+ * @CSDN :blog.csdn.net/Sirius_hly
+ * @WeChat :xcvip2021
+ * @date :2021/5/18
+ * @desc : 32 - I. 从上到下打印二叉树
+ * 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+ */
+public class LevelOrder {
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public int[] levelOrder(TreeNode root) {
+        if (root == null) {
+            return new int[]{};
+        }
+        List<Integer> list = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            list.add(cur.val);
+            if (cur.left != null) {
+                queue.offer(cur.left);
+            }
+            if (cur.right != null) {
+                queue.offer(cur.right);
+            }
+        }
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
