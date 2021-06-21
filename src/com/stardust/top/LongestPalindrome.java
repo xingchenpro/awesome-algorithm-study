c96cb9da5a2699335f0a2d7f8b31b05aba5d8ab4
huangliangyun <1136513099@qq.com>
2021-06-21 23:04:07 +0800

    6.21

---
+++ src/com/stardust/top/LongestPalindrome.java
@@ -0,0 +1,58 @@
+package com.stardust.top;
+
+/**
+ * @author :hly
+ * @CSDN :blog.csdn.net/Sirius_hly
+ * @date :2021/6/21
+ * @desc : 5. 最长回文子串
+ * 给你一个字符串 s，找到 s 中最长的回文子串。
+ */
+public class LongestPalindrome {
+
+    //滑动窗口
+    public String longestPalindrome(String s) {
+        if (s.length() == 1) {
+            return s;
+        }
+        int size = s.length();
+        while (size>=1){
+            //右指针
+            int left = 0;
+            do {
+                String t = s.substring(left,left+size);
+                if(isPalind(t)){
+                    return t;
+                }
+                left++;
+            }while (left+size<=s.length());
+            size--;
+        }
+        return "";
+    }
+
+    private boolean isPalind(String s) {
+        int i = 0, j = s.length() - 1;
+        //i<j,否则如果是回文，会往回倒，溢出
+        while (i < j && s.charAt(i) == s.charAt(j)) {
+            i++;
+            j--;
+        }
+        //奇数个数，i=j,偶数个数,i>j。如果不是回文，循环就停止了，i永远小于j
+        return i >= j;
+    }
+
+    //动态规划
+
+    public static void main(String[] args) {
+
+        LongestPalindrome longestPalindrome = new LongestPalindrome();
+        System.out.println(longestPalindrome.isPalind("abcba"));
+        System.out.println(longestPalindrome.isPalind("abcbc"));
+
+        System.out.println(longestPalindrome.longestPalindrome("babad"));
+        System.out.println(longestPalindrome.longestPalindrome("cbbd"));
+        System.out.println(longestPalindrome.longestPalindrome("bb"));
+
+    }
+
+}
